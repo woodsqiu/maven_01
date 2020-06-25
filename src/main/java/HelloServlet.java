ddbfdafa20a0435a7bcc017d7d1733c7ca3e2199
woodsqiu <wangzhiqiu1987@163.com>
2020-06-25 11:48:19 +0800

    ceshi

---
+++ src/main/java/HelloServlet.java
@@ -0,0 +1,25 @@
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * @author wangzhiqiu
+ * @since 20/6/25 上午11:16
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write(new Date().toString());
+    }
+}
